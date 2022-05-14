fcba5558dec74ccb77eef7945019b5fcc102794a
Nikolai <cooprix1@gmail.com>
2022-05-14 10:35:26 +0200

    Charity Cards complete!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import CharityCard from "./components/CharityCard";
+import icon from "./images/icon.jpeg"
+
 
 
 function App() {
-  const myElement = {name: "Ford", title: "A"};
+  const myElement = {
+    name: "Ford", 
+    title: "Education 4All",
+    description: "Our donation goal of building 10 more schools in Africa has been achieved. Meloria Impact are grateful for all...",
+    daysleft: 11,
+    completeness: 3,
+    image: icon
+  };
   //<CharityCard name={myElement.name} title="ho"/>
   
   return (
--- src/components/CharityCard.js
+++ src/components/CharityCard.js
@@ -1,23 +1,22 @@
 import Charity from "../css/Charity.css"
-import icon from "../images/icon.jpeg"
 import buildingicon from "../images/building-icon.png"
 
 
-function CharityCard({name, title}) {
+function CharityCard({name, image, title, daysleft, completeness, description}) {
   
   return (
     <div className="education-card">
-      <img className="image" src={icon} alt="image" />
-      <p className="daysleft">{name} 12 Days Left</p>
+      <img className="image" src={image} alt="image" />
+      <p className="daysleft">{daysleft} Days Left</p>
       <div className="stats">
         <img src={buildingicon} classname="buildingicon" />
-        <p>8/10</p>
+        <p>{completeness}/10</p>
         <p>∙</p>
-        <p>80% complete</p>
+        <p>{completeness}0% complete</p>
       </div>
-      <h1 className="card-title">{title}Education 4 all</h1>
+      <h1 className="card-title">{title}</h1>
 
-      <p className="card-description">Our donation goal of building 10 more schools in Africa has been achieved. Meloria Impact are grateful for all...
+      <p className="card-description">{description}
         <p className="readmore">Read more</p>
       </p>
       <>
