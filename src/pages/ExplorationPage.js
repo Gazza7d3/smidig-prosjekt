ba396e4dfab9e6eae8faa23ec7c5f74529a2e646
Nikolai <cooprix1@gmail.com>
2022-05-24 18:52:53 +0200

    Added hero and centered items

--- src/pages/ExplorationPage.js
+++ src/pages/ExplorationPage.js
@@ -1,6 +1,7 @@
 import CharityCard from "../Components/CharityCard";
 import icon from "../images/icon.jpeg"
 import icon2 from "../images/bk.webp"
+import hero from "../images/Hero.png"
 import styled from 'styled-components';
 
 
@@ -10,17 +11,22 @@ import styled from 'styled-components';
 
 function ExplorationPage() {
     return (
-      <body style={{display: "flex", flex: 1, justifyContent: "center"}}>
-          <nav>Hey</nav>
-          <header>Pic</header>
-          <div style={{width: "1120px", border: "1px solid black", borderColor: "black"}}>
-          <div style={{display: "flex", flex: 4, flexWrap: "wrap"}}>
+      <body>
+        <main>
+        <img style={ExploarationStyles.hero} src={hero} />
+        <h1 style={{display: "flex", justifyContent: "center"}}>Explore Charities</h1>
+        </main>
+
+      <div style={{display: "flex", justifyContent: "center"}}>
+      <div style={{width: "1120px"}}>
+          <div style={{display: "flex", flex: 4, flexWrap: "wrap", justifyContent: "center"}}>
               <CharityCard {...myElement}/>
               <CharityCard {...myElement}/>
               <CharityCard {...myNewElement}/>
               <CharityCard {...myNewElement}/>
               <CharityCard {...myNewElement}/>
               <CharityCard {...myNewElement}/>
+              </div>
           </div>
           </div>
       </body>
@@ -29,6 +35,8 @@ function ExplorationPage() {
 
   export default ExplorationPage;
 
+
+  /* Elements */
   const myElement = {
     title: "Education 4All",
     description: "Our donation goal of building 10 more schools in Africa has been achieved. Meloria Impact are grateful for all...",
@@ -51,4 +59,18 @@ function ExplorationPage() {
     daysleft: 15,
     completeness: 7,
     image: icon2
+  }
+
+  /* Styling */
+
+  export const ExploarationStyles = {
+    hero: {
+      width: "100%",
+      height: "50%",
+
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      position: "relative"
+    }
   }
